6315266dc50e1c3615dcb9faecb6b13a36d4515a
SnyderMbishai <csmbishai@gmail.com>
2018-06-28 02:42:59 +0300

    Add import helper

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import psycopg2
 from flask import Flask
 from flask_restful import Api
+import sys  # handle imports
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api.views import Signup, Login, Rides, Users, Logout
 
 from config import configurations
@@ -17,7 +20,7 @@ def create_app(configuration):
 
     api.add_resource(Signup, '/api/v2/auth/signup')
     api.add_resource(Login, '/api/v2/auth/login')
-    api.add_resource(Logout,'api/v2/auth/logout')
+    # api.add_resource(Logout, '/api/v2/auth/logout')
     api.add_resource(Rides, '/api/v2/rides', 
                         '/api/v2/rides/<int:ride_id>', 
                         '/api/v2/rides/<int:ride_id>/requests')
@@ -38,5 +41,5 @@ def connect_to_db():
 
 
 app = create_app('development')
-conn = connect_to_db()
+
 
