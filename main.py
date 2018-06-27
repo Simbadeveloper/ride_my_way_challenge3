3e9d95f0a4656b07e7181fa1a3838e92062c1124
SnyderMbishai <csmbishai@gmail.com>
2018-06-28 01:55:46 +0300

    Add logout URL

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import psycopg2
 from flask import Flask
 from flask_restful import Api
 from api.views import Signup, Login, Rides, Users, Logout
@@ -10,12 +11,13 @@ def create_app(configuration):
     app = Flask(__name__)
 
     app.config.from_object(configurations[configuration])
-     
+    
+
     api = Api(app)
 
     api.add_resource(Signup, '/api/v2/auth/signup')
     api.add_resource(Login, '/api/v2/auth/login')
-    api.add_resource(Logout, '/api/v2/auth/logout')
+    api.add_resource(Logout,'api/v2/auth/logout')
     api.add_resource(Rides, '/api/v2/rides', 
                         '/api/v2/rides/<int:ride_id>', 
                         '/api/v2/rides/<int:ride_id>/requests')
@@ -26,6 +28,15 @@ def create_app(configuration):
         
     return app
 
+def connect_to_db():
+    connection = 'dbname=development user=carpool password=carpool host=localhost '
+    print (connection)
+    try:
+        return psycopg2.connect(connection)
+    except:
+        print("cant connect")
+
 
 app = create_app('development')
+conn = connect_to_db()
 
