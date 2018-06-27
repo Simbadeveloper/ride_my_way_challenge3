52c28d9260b07ffac5367cc6dccc6f48f3e92dc1
SnyderMbishai <csmbishai@gmail.com>
2018-06-27 16:00:47 +0300

    add more routes and rename the ids differently

--- main.py
+++ main.py
@@ -10,22 +10,18 @@ def create_app(configuration):
     app = Flask(__name__)
 
     app.config.from_object(configurations[configuration])
-    app_context = app.app_context()
-
-    app_context.push()
-    
-
+     
     api = Api(app)
 
     api.add_resource(Signup, '/api/v2/auth/signup')
     api.add_resource(Login, '/api/v2/auth/login')
     api.add_resource(Logout, '/api/v2/auth/logout')
     api.add_resource(Rides, '/api/v2/rides', 
-                        '/api/v2/rides/<int:id>', 
-                        '/api/v2/rides/<int:id>/requests')
+                        '/api/v2/rides/<int:ride_id>', 
+                        '/api/v2/rides/<int:ride_id>/requests')
     api.add_resource(Users, '/api/v2/users/rides',
-                            '/api/v2/users/rideID/requests', 
-                            '/api/v2/users/requests/reqID')
+                            '/api/v2/users/rides/<int:ride_id>/requests', 
+                            '/api/v2/users/rides/<int:ride_id>requests/<int:request_id>')
 
         
     return app
