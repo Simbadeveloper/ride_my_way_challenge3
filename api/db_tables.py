7c6a9d2417fbeb6c606a65d0ae4550a5992032fd
SnyderMbishai <csmbishai@gmail.com>
2018-06-29 07:46:34 +0300

    Update db_tables.py

--- api/db_tables.py
+++ api/db_tables.py
@@ -1,8 +1,11 @@
 import psycopg2
+import os
 
 def connect_to_db():
     """  Method for connecting to the database.  """
-
+    DB_URL = os.getenv('DATABASE_URL', None)
+    if DB_URL:
+        return psycopg2.connect(DB_URL)
     connection = 'dbname=development user=postgres password=password77 host=localhost '
     print (connection)
     try:
